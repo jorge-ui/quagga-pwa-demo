336d0022de2d55fa563d8ae5eeb2129d5e4a436b
Jorge Rivera <jorge.rivera@innovatiopr.com>
2024-04-07 14:16:46 -0400

    Update Vite configuration to document PWA features

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,18 @@ import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+/**
+ * Vite configuration file.
+ * @remarks This file exports the configuration object for Vite, the build tool for modern web applications.
+ * @see {@link https://vitejs.dev/config/}
+ */
 export default defineConfig({
 	plugins: [
 		react(),
+		/**
+		 * Vite plugin for enabling PWA features.
+		 * @see {@link https://vite-pwa-org.netlify.app/guide/}
+		 */
 		VitePWA({
 			registerType: "autoUpdate",
 			includeAssets: ["favicon.ico", "robots.txt", "apple-touch-icon.png"],
