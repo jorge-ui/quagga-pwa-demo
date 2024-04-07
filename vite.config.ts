d29016d5b20c64f2cf62d4450c4ab3be05517015
Jorge Rivera <jorge.rivera@innovatiopr.com>
2024-04-07 00:14:46 -0400

    Add dev-dist to .gitignore, update devDependencies in package.json, and configure VitePWA in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [
+		react(),
+		VitePWA({
+			registerType: "autoUpdate",
+			includeAssets: ["favicon.ico", "robots.txt", "apple-touch-icon.png"],
+			manifest: {
+				name: "Barcode Scanner",
+				short_name: "Barcode Scanner",
+				theme_color: "#46d2c0",
+			},
+			devOptions: {
+				enabled: process.env.NODE_ENV === "development",
+			},
+		}),
+	],
+});
